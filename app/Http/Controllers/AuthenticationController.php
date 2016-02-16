2bef1cebc94ddd9f10c06f3137d83a15c707c7d2
Michael Kroll <michael.kroll.94@googlemail.com>
2016-02-16 22:30:30 +0100

    Login Methode

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use SmartBell\Http\Requests;
 use SmartBell\Http\Controllers\Controller;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthenticationController extends Controller
 {
@@ -24,7 +26,20 @@ class AuthenticationController extends Controller
     }
 
     public function postLogin(Request $req){
-        //TODO implement
+        $credentials = $req->only('email', 'password');
+
+        try {
+            // verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // if no errors are encountered we can return a JWT
+        return response()->json(compact('token'));
     }
 
     public function postLogout(Request $req){
