0559e613478e36550e17bd13b30ff0e8929d3c87
Michael Kroll <michael.kroll.94@googlemail.com>
2016-02-17 11:15:47 +0100

    Tippfehler

--- database/migrations/2016_02_16_143053_create_push_clients_table.php
+++ database/migrations/2016_02_16_143053_create_push_clients_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePushclientsTable extends Migration
+class CreatePushClientsTable extends Migration
 {
     /**
      * Run the migrations.
