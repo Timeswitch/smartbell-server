2332569e8f6bc067aadab1674bfb2f54797ce039
Michael Kroll <michael.kroll.94@googlemail.com>
2016-02-24 11:50:43 +0100

    webworker fix

--- public/gcm.js
+++ public/gcm.js
@@ -27,10 +27,33 @@
             }).then(function(data){
                 return self.registration.showNotification('SmartBell',{
                     'body': data.bell,
-                    'icon': 'img/uploads/'+data.image
+                    'icon': 'img/uploads/'+data.image,
+                    'tag': data.id
                 });
             });
 
         }));
     });
+
+    self.addEventListener('notificationclick', function(event) {
+        event.notification.close();
+        var url = 'https://smartbell.kroll.zone/#/rings/'+event.notification.tag;
+        event.waitUntil(
+            clients.matchAll({
+                    type: 'window'
+                })
+                .then(function(windowClients) {
+                    for (var i = 0; i < windowClients.length; i++) {
+                        var client = windowClients[i];
+                        if (client.url === url && 'focus' in client) {
+                            return client.focus();
+                        }
+                    }
+                    if (clients.openWindow) {
+                        return clients.openWindow(url);
+                    }
+                })
+        );
+    });
+
 })();
